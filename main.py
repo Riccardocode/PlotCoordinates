344fe827e032cac09d02f495494c55ed08591c3e
Riccardo-dubug <52591461+Riccardo-dubug@users.noreply.github.com>
2022-12-27 12:02:17 +0100

    Self centered map with first coordinates of the file (first line of the file) Aggiunta validazione delle coordinate per non far crashare il programma in caso di coordinate corrotte

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+#read the coordinates from a file.
+#each line of the file contains: 0 or 1 , lat, lon
+#first value is zero if no materials have been detected and 1 if any of the listed material has been detected
+#in this particular application, 0 is no plastic detected and 1 is plastic detected
+
 import folium
 from folium.plugins import MarkerCluster
 import csv
 from selenium import webdriver
 import time
-
 
 layerControl = 0
 x = "C:\plotcoordinates\my_map.html"
@@ -21,10 +25,12 @@ m = folium.Map(location=[lat, lon], zoom_start=12)
 # Aggiungo i due cluster (plastica e non) alla mappa
 PlasticaCluster = MarkerCluster(name="Plastica").add_to(m)
 NoPlasticaCluster = MarkerCluster(name="No-Plastica").add_to(m)
-m.save(x)  # salvo la mappa prima di impostare selenium
+m.save(x)  # salvo la mappa
 
-driver = webdriver.Firefox()
-driver.get(x)
+#todo: aggiungere scelta dell'aggiornamento automatico da parte dell'utente
+#Commentare le prossime due righe se si vuole disattivare l'aggiornamento automatico (commentare anche un altra riga sotto)
+driver = webdriver.Firefox()            #allorazione del webdriver per il refresh automatico.
+driver.get(x)                           #prende la mappa e la aggiorna in automatico.
 
 while True:
     N = len(results)
@@ -32,7 +38,7 @@ while True:
     # for line in (csvfile.readlines()[N:]):
     # print(line)
     # results.append(line)
-    reader = csv.reader(csvfile.readlines()[N:], quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    reader = csv.reader(csvfile.readlines()[N:])  # change contents to floats
     for row in reader:  # each row is a list
         results.append(row)
     print(results)
@@ -40,20 +46,49 @@ while True:
 
     if len(results) > 0:
         for cords in results[N:]:
-            if cords[0] == 1.0:
-                folium.CircleMarker(location=[cords[1], cords[2]], radius=3, popup="{0},{1}".format(cords[1], cords[2]),
-                                    color="red", icon=folium.Icon(icon_color='red')).add_to(PlasticaCluster)
+
+            #here starts the validation: for each line the 3 values are being validated
+            if len(cords) == 3:
+                if cords[0].isdigit():
+                    contadigits = 0
+                    for c in cords[1]:
+                        if c.isdigit():
+                            contadigits += 1
+                    contapunti = cords[1].count(".")
+                    if contapunti + contadigits == len(cords[1]):
+                        contadigits = 0
+                        for c in cords[2]:
+                            if c.isdigit():
+                                contadigits += 1
+                        contapunti = cords[2].count(".")
+                        if contapunti + contadigits == len(cords[2]):
+                            if float(cords[1]) >= -90 and float(cords[1]) <= 90 and float(cords[2]) >= -180 and float(cords[2]) <= 180:
+                                if int(cords[0]) == 1:
+                                    folium.CircleMarker(location=[cords[1], cords[2]], radius=3,
+                                                        popup="{0},{1}".format(cords[1], cords[2]),
+                                                        color="red", icon=folium.Icon(icon_color='red')).add_to(PlasticaCluster)
+                                elif int(cords[0]) == 0:
+                                    folium.CircleMarker(location=[cords[1], cords[2]], radius=3,
+                                                        popup="{0},{1}".format(cords[1], cords[2]),
+                                                        color="green", icon=folium.Icon(icon_color='green')).add_to(NoPlasticaCluster)
+                                else:
+                                    print("coordinates discarded")
+                            else:
+                                print("coordinates discarded")
+                        else: print("coordinates discarded")
+                    else: print("coordinates discarded")
+
+                else:
+                    print("coordinates discarded")
             else:
-                folium.CircleMarker(location=[cords[1], cords[2]], radius=3, popup="{0},{1}".format(cords[1], cords[2]),
-                                    color="green", icon=folium.Icon(icon_color='green')).add_to(NoPlasticaCluster)
+                print("coordinates discarded")
 
     # Aggiungo il controlLayer alla mappa
     if (layerControl == 0):
         folium.LayerControl().add_to(m)
-        layerControl=1
+        layerControl = 1
     m.save(x)
-    # results.clear()
-    # Refresh time with Selenium
-    time.sleep(refreshrate)
-    driver.refresh()
 
+
+    time.sleep(refreshrate)
+    driver.refresh()    # refresh page automatico. commentare questa riga per disattivarlo (commentare anche altre due righe sopra)
